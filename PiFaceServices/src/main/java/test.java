c11b2531128e9e7128362032b7fe0959370af917
rewati <rewati.raman@gmail.com>
2016-03-22 22:57:01 -0700

    Initial commit

---
+++ PiFaceServices/src/main/java/test.java
@@ -0,0 +1,24 @@
+import com.pi4j.device.piface.PiFace;
+import com.pi4j.device.piface.impl.PiFaceDevice;
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.io.spi.SpiChannel;
+
+import java.io.IOException;
+
+/**
+ * Created by Rewati Raman(rewati.raman@gmail.com).
+ */
+public class test {
+    public static void main(String[] arg) throws IOException {
+
+        final GpioController gpio = GpioFactory.getInstance();
+
+        // provision gpio pin #01 & #03 as an output pins and blink
+        final GpioPinDigitalOutput led1 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01);
+        led1.blink(500);
+    }
+
+}
