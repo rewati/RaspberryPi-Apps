011343d66f1ad83fc145fa16ad881136d71564be
rewati <rewati.raman@gmail.com>
2016-04-01 16:18:24 -0700

    AutoBot1 project Created

---
+++ AutoBot1/src/main/java/com/rr/autobot1/Application.java
@@ -0,0 +1,14 @@
+package com.rr.autobot1;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Rewati Raman(rewati.raman@gmail.com).
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String arg[]) {
+        System.out.println("Hello there");
+    }
+}
