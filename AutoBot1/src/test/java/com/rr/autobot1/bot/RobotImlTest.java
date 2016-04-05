ad0079bc53b951c78daced5cf82a3aed41d7470e
rewati <rewati.raman@gmail.com>
2016-04-05 02:04:56 -0700

    Empty unit test class added

---
+++ AutoBot1/src/test/java/com/rr/autobot1/bot/RobotImlTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2016 Rewati Raman(rewati.raman@gmail.com) 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.rr.autobot1.bot;
+
+
+import org.junit.Test;
+
+/**
+ * Created by Rewati Raman(rewati.raman@gmail.com).
+ */
+public class RobotImlTest {
+
+    @Test
+    public void init() throws Exception {
+
+    }
+
+    @Test
+    public void turnLeft() throws Exception {
+
+    }
+
+    @Test
+    public void turnRight() throws Exception {
+
+    }
+
+    @Test
+    public void stop() throws Exception {
+
+    }
+
+    @Test
+    public void moveForward() throws Exception {
+
+    }
+
+    @Test
+    public void reverse() throws Exception {
+
+    }
+}
